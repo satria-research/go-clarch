5b4cc53e792ed9789a2785ee01cdcfbd5e08fbdd
Ubaidillah Hakim <hakim.ubaidillah@sunnysideup.io>
2024-01-08 20:33:08 +0700

    kk

--- app/infra/config/glitchtip.go
+++ app/infra/config/glitchtip.go
@@ -6,8 +6,6 @@ import (
 
 func GlitchtipInit(configuration IConfig) {
 	sentry.Init(sentry.ClientOptions{
-		Dsn:              configuration.Get("GLITCHTIP_DSN"),
-		EnableTracing:    true,
-		TracesSampleRate: 1.0,
+		Dsn: configuration.Get("SENTRY_DSN"),
 	})
 }
